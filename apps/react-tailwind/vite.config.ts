65d2367ac39d1f411788cb7b848b073d33567b7f
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-19 05:50:15 +0600

    Update dependency

--- apps/react-tailwind/vite.config.ts
+++ apps/react-tailwind/vite.config.ts
@@ -1,10 +1,5 @@
-import { platform } from "node:os"
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
 
-const path = `${import.meta.dirname}`.replaceAll("\\", "/").split("/").reverse()
-export default defineConfig({
-    base: platform() !== "win32" ? `${path[1]}/${path[0]}` : "./",
-    plugins: [react(), tailwindcss()]
-})
+export default defineConfig({ plugins: [react(), tailwindcss()] })
